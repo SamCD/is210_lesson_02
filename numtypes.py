685de5a5bcebd37339d51a7bffdd5ba36452e78f
apag101 <apag101@yahoo.com>
2014-09-07 18:43:33 -0400

    Homework 2 corrections 12

--- numtypes.py
+++ numtypes.py
@@ -6,8 +6,8 @@ from decimal import Decimal
 from fractions import Fraction
 
 FLOATVAR = 0.1
-DECIMALVAR = Decimal(0.1)
-FRACTIONVAR = Fraction(1/10)
+DECIMALVAR = Decimal(1, 10)
+FRACTIONVAR = Fraction(1, 10)
 
 DF_EQUALITY = DECIMALVAR == FRACTIONVAR
 ARE_INEQUAL = DECIMALVAR != FRACTIONVAR != FLOATVAR
