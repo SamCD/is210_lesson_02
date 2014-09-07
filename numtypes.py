10d77039ce5ad091d9c56369a9334fa82c17402c
Kevin Doles <kevin.doles@spsmail.cuny.edu>
2014-09-07 14:30:13 -0400

    Created file numtypes.py for Task 15.

---
+++ numtypes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/evn python
+#-*- UTF-8 -*-
+
+FLOATVAR = 0.1
+import decimal
+from decimal import*
+DECIMALVAR = Decimal(0.1)
+import fraction
+from fractions import Fraction
+FRACTIONVAR = FRACTION(1, 10)
