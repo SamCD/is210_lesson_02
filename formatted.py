81cc6c68fb474de3989d449805225bf13a1a3254
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-07 18:43:00 -0400

    completed the tasks

--- formatted.py
+++ formatted.py
@@ -7,11 +7,7 @@ FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = str(42).zfill(6)
 
-#Task 19
 NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
 EMAIL = NEWS.format(NTYPE, RNUM, friend=FNAME)
 print EMAIL
 
-#I couldn't figure out how to add the leading zeros on the NEWS variable,
-#but was able to add them using .zfill() on the RNUM variable.
-#Hopefully this is acceptable!  Thank you!
