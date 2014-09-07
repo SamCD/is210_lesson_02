bfe569abaafe729d5919704364c0c8b0a7fd312a
apag101 <apag101@yahoo.com>
2014-09-07 19:16:30 -0400

    Homework 2 corrections 17

--- formatted.py
+++ formatted.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-FNAME = 'Alex'
+FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
 NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:06.0f}!'
