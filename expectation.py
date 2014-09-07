0221e729d338b13527950535dbe7a2d940b4665e
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-07 18:08:05 -0400

    completed the tasks

--- expectation.py
+++ expectation.py
@@ -2,5 +2,10 @@
 # -*- coding: utf-8 -*-
 """Contains expectations."""
 
-
+#Task 10
 import inquisition
+inquisition.SPANISH
+
+SPANISH = inquisition.SPANISH
+FISHY = SPANISH.replace('surprise', 'haddock')
+print FISHY
