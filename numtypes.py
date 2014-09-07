c6b2fbe1e71f8ad15fd0232f5dfbc5e5ebb0297b
apag101 <apag101@yahoo.com>
2014-09-07 19:00:27 -0400

    Homework 2 corrections 14

--- numtypes.py
+++ numtypes.py
@@ -6,7 +6,8 @@ from decimal import Decimal
 from fractions import Fraction
 
 FLOATVAR = 0.1
-DECIMALVAR = Decimal(.01)
+DECIMALVAR0 = Decimal(.01)
+DECIMALVAR = '{0:4.2f}'.format(DECIMALVAR0)
 FRACTIONVAR = Fraction(1, 10)
 
 DF_EQUALITY = DECIMALVAR == FRACTIONVAR
