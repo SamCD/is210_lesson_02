34abc845d575ab62aa550599af4afa5c6746c357
Joanne Garcia <joanne.garcia@spsmail.cuny.edu>
2014-09-07 18:36:22 -0400

    completed the tasks

--- artists.py
+++ artists.py
@@ -6,23 +6,18 @@
 THE_GREAT_QUESTION = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
                       'Creators of the great works? Both? You be the judge!')
 
-#Task 4
 STATEMENTS = THE_GREAT_QUESTION.split('. ')
 print STATEMENTS
 
-#Task 5
 ARTISTS = STATEMENTS[0:4]
 print ARTISTS
 
-#Task 6 part 1
 NUM_ARTISTS = len(ARTISTS)
 print NUM_ARTISTS
 
-#Task 6 part 2
 CHARACTERS = len(THE_GREAT_QUESTION)
 print CHARACTERS
 
-#Task 7
 HAS_CREATORS = 'Creators' in THE_GREAT_QUESTION
 print HAS_CREATORS
 
