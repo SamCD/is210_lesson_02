b8e238f12669867b179cb5b52021981e8ec98439
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-07 23:38:38 -0400

    My task 19 answers

--- formatted.py
+++ formatted.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'.format("*amazing*", "000042", friend="Pat")
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'.format("*amazing*",
+"000042", friend="Pat")
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
-EMAIL = NEWS.format(NTYPE, RNUM, friend= FNAME)
+EMAIL = NEWS.format(NTYPE, RNUM, friend=FNAME)
