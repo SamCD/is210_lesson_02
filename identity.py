98821548d53a849bd083abfcc759a54fa058d7e5
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-08 01:01:43 -0400

    My task 20 answers

--- identity.py
+++ identity.py
@@ -62,7 +62,7 @@ def is_empty(my_sequence):
     count = get_member_count(my_sequence)
 
     if count == False:
-        return count is 0
+        return count is None
     else:
         raise TypeError('Object has no len()')
 
