1a773e71cd09bfa8c0de173ad9409dc97f413734
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-07 22:31:58 -0400

    My task 15 answers

---
+++ numtypes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Numeric Types'''
+
+from decimal import Decimal
+from fractions import Fraction
+
+FLOATVAR = 0.1
+DECIMALVAR = Decimal('0.1')
+FRACTIONVAR = Fraction (1,10)
