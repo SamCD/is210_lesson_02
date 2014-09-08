3763c53942a572e0632c5fdf9485aed5f81d4606
Kevin Doles <kevin.doles@spsmail.cuny.edu>
2014-09-07 22:00:27 -0400

    First list of corrections to Assignment 02.

--- flemish.py
+++ flemish.py
@@ -7,5 +7,5 @@ import inquisition
 FISHY = inquisition.SPANISH.replace('surprise', 'haddock')
 ONE_FISH = 'Spanish'
 TWO_FISH = len(ONE_FISH)
-SPANISH_FISHY = expectation.FISHY.index(ONE_FISH, 0, len(expectation.FISHY))
-FLEMISH_INQUISITION = expectation.FISHY[:19] + 'Flemish' + expectation.FISHY[26:]
+SPANISH_FISHY = FISHY.index(ONE_FISH, 0, len(expectation.FISHY))
+FLEMISH_INQUISITION = FISHY[:19] + 'Flemish' + expectation.FISHY[26:]
--- numtypes.py
+++ numtypes.py
@@ -1,5 +1,5 @@
-#!/usr/bin/evn python
-#-*- UTF-8 -*-
+# !/usr/bin/evn python
+# -*- UTF-8 -*-
 
 FLOATVAR = 0.1
 import decimal
@@ -7,6 +7,6 @@ from decimal import*
 DECIMALVAR = Decimal(0.1)
 import fraction
 from fractions import Fraction
-FRACTIONVAR = FRACTION(1, 10)
+FRACTIONVAR = Fraction(1, 10)
 DF_EQUALITY = DECIMALVAR == FRACTIONVAR
 ARE_INEQUAL = FLOATVAR != DECIMALVAR != FRACTIONVAR
